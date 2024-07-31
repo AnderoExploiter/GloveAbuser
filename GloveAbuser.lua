10b2f8e78503f9d0a8feb97eb7ac55790bca01ce
AnderoExploiter <anderoexploits@gmail.com>
2024-07-31 18:08:07 +0700

    idk

--- GloveAbuser.lua
+++ GloveAbuser.lua
@@ -1,1 +1,18 @@
-print(2)
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Glove Abuser", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+
+local MainTab = Window:MakeTab({
+	Name = "Main",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local SlapSection = MainTab:AddSection({
+	Name = "Slap Gloves"
+})
+
+local SlapSection = MainTab:AddSection({
+	Name = "Slap Gloves"
+})
+

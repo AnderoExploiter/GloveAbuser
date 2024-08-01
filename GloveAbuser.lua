2bab24d27b23f6e02c68a8e2846993199bb766bb
AnderoExploiter <anderoexploits@gmail.com>
2024-08-01 18:25:30 +0700

    123

--- GloveAbuser.lua
+++ GloveAbuser.lua
@@ -1,4 +1,3 @@
-print("[-------------------------]")
 print("Made By N | O | T | S | I | V | Y")
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 

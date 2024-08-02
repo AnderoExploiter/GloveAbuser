3ab11afa914788b1d53d845a292aa64ad2904141
AnderoExploiter <anderoexploits@gmail.com>
2024-08-02 19:00:46 +0700

    Create ActivateSlicer.lua

---
+++ Events/ActivateSlicer.lua
@@ -0,0 +1,2 @@
+game:GetService("ReplicatedStorage").Slicer:FireServer("sword")
+game:GetService("ReplicatedStorage").Slicer:FireServer("slash", game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame, Vector3.new())

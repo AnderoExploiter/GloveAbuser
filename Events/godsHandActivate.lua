b9b8b7359410032375edc887af18f033921de1e5
AnderoExploiter <anderoexploits@gmail.com>
2024-08-02 19:04:14 +0700

    Create godsHandActivate.lua

---
+++ Events/godsHandActivate.lua
@@ -0,0 +1,3 @@
+game:GetService("ReplicatedStorage").TimestopJump:FireServer()
+game:GetService("ReplicatedStorage").Timestopchoir:FireServer()
+game:GetService("ReplicatedStorage").Timestop:FireServer()

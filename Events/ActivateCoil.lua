325b289ea0f1092892868e5b86a454f2def12b51
AnderoExploiter <anderoexploits@gmail.com>
2024-08-02 18:51:52 +0700

    Create ActivateCoil.lua

---
+++ Events/ActivateCoil.lua
@@ -0,0 +1,2 @@
+game:GetService("ReplicatedStorage"):WaitForChild("GeneralAbility"):FireServer(game:GetService("Players").LocalPlayer.Character.Coil)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Walkspeed
